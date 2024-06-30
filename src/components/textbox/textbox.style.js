a063a467821af4ad53329de5b630e898337d001f
fernandoberbel <fberbel1000@gmail.com>
2024-06-30 15:59:20 -0300

    feat: add component textbox

---
+++ src/components/textbox/textbox.style.js
@@ -0,0 +1,19 @@
+import { COLORS, FONT_SIZE } from "../../constants/theme.js";
+
+export const styles = {
+  input: {
+    width: "100%",
+    // height: 40,
+    backgroundColor: COLORS.white,
+    padding: 14,
+    borderRadius: 6,
+    borderWidth: 1,
+    borderColor: COLORS.light_gray,
+  },
+  label: {
+    marginLeft: 5,
+    marginBottom: 4,
+    color: COLORS.dark_gray,
+    fontSize: FONT_SIZE.md,
+  },
+};
