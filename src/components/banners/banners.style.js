001e5ba74b831569a318a8690010ee2ab42441c9
fernandoberbel <fberbel1000@gmail.com>
2024-07-18 22:12:08 -0300

    feat: created component banners to home page

---
+++ src/components/banners/banners.style.js
@@ -0,0 +1,18 @@
+import { COLORS, FONT_SIZE } from "../../constants/theme.js";
+
+export const styles = {
+  icon: {
+    width: 220,
+    height: 120,
+  },
+  banner: {
+    paddingRight: 10,
+    marginTop: 30,
+    marginBottom: 30,
+  },
+  description: {
+    fontSize: FONT_SIZE.xsm,
+    color: COLORS.dark_gray,
+    textAlign: "center",
+  },
+};
