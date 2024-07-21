67a7f37117c39c25bd05a1c590027ed40441be30
fernandoberbel <fberbel1000@gmail.com>
2024-07-20 22:22:50 -0300

    feat: created favorite page

---
+++ src/screens/favorite/favorite.jsx
@@ -0,0 +1,38 @@
+import { FlatList, Image, Text, View } from "react-native";
+import { styles } from "./favorite.style.js";
+import { restaurantes } from "../../constants/data.js";
+import Restaurant from "../../components/restaurant/restaurant.jsx";
+import icons from "../../constants/icons.js";
+
+function Favorite() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={restaurantes}
+        keyExtractor={(restaurante) => restaurante.id}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => {
+          return (
+            <Restaurant
+              logotipo={item.logotipo}
+              nome={item.nome}
+              endereco={item.endereco}
+              icone={icons.remove}
+            />
+          );
+        }}
+        contentContainerStyle={styles.containerList}
+        ListEmptyComponent={() => {
+          return (
+            <View style={styles.empty}>
+              <Image source={icons.empty} />
+              <Text style={styles.emptyText}>Nenhum favorito encontrado</Text>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+}
+
+export default Favorite;
---
+++ src/screens/favorite/favorite.style.js
@@ -0,0 +1,22 @@
+import { COLORS, FONT_SIZE } from "../../constants/theme.js";
+
+export const styles = {
+  container: {
+    flex: 1,
+    padding: 12,
+    backgroundColor: COLORS.white,
+  },
+  containerList: {
+    flexGrow: 1,
+  },
+  empty: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 100,
+  },
+  emptyText: {
+    color: COLORS.dark_gray,
+    fontSize: FONT_SIZE.sm,
+  },
+};
