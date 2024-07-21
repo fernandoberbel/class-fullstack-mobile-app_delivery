2c91c0642011a8cd8e4694f0f01b54a97d4de986
fernandoberbel <fberbel1000@gmail.com>
2024-07-20 22:14:33 -0300

    feat: add routes Favorite, Orders and Profile

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,9 @@ import Login from "./screens/login/login.jsx";
 import Registro from "./screens/registro/registro.jsx";
 import Registro2 from "./screens/registro2/registro2.jsx";
 import Home from "./screens/home/home.jsx";
+import Favorite from "./screens/favorite/favorite.jsx";
+import Orders from "./screens/orders/orders.jsx";
+import Profile from "./screens/profile/profile.jsx";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +15,33 @@ function Routes() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen
+          name="profile"
+          component={Profile}
+          options={{
+            title: "Meu Perfil",
+            headerTitleAlign: "center",
+            headerShadowVisible: false,
+          }}
+        />
+        <Stack.Screen
+          name="orders"
+          component={Orders}
+          options={{
+            title: "Meus Pedidos",
+            headerTitleAlign: "center",
+            headerShadowVisible: false,
+          }}
+        />
+        <Stack.Screen
+          name="favorite"
+          component={Favorite}
+          options={{
+            title: "Favoritos",
+            headerTitleAlign: "center",
+            headerShadowVisible: false,
+          }}
+        />
         <Stack.Screen
           name="home"
           component={Home}
