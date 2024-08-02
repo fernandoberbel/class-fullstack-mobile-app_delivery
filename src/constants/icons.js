ba8ab83745120b3bf2d52c80420093959ee35e3d
fernandoberbel <fberbel1000@gmail.com>
2024-08-02 18:55:11 -0300

    feat: add icons

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,4 +1,5 @@
 import back from "../assets/back.png";
+import back2 from "../assets/back2.png";
 import remove from "../assets/delete.png";
 import logo from "../assets/logo.png";
 import cart from "../assets/cart.png";
@@ -13,6 +14,7 @@ import abaHome from "../assets/aba-home.png";
 import abaFavorito from "../assets/aba-favorito.png";
 import abaPedidos from "../assets/aba-pedidos.png";
 import abaPerfil from "../assets/aba-perfil.png";
+import location from "../assets/location.png";
 
 export default {
   back,
@@ -30,4 +32,6 @@ export default {
   abaFavorito,
   abaPedidos,
   abaPerfil,
+  back2,
+  location,
 };
