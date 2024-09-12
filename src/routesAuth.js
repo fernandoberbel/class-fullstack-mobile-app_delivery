cdfd48bf5e562379044223177d10eb2b51f58079
fernandoberbel <fberbel1000@gmail.com>
2024-09-12 01:13:43 -0300

    feat: add checkout route

--- src/routesAuth.js
+++ src/routesAuth.js
@@ -1,11 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Text, TouchableOpacity } from "react-native";
+
+import { COLORS } from "./constants/theme.js";
 
 import Primary from "./screens/primary/primary.jsx";
 import Menu from "./screens/menu/menu.jsx";
 import Search from "./screens/search/search.jsx";
 import ProductDetail from "./screens/detalhe-produto/product-detail.jsx";
 import OrderDetail from "./screens/order-detail/order-detail.jsx";
+import Checkout from "./screens/checkout/checkout.jsx";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,18 +18,40 @@ function RoutesAuth() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
+          name="primary"
+          component={Primary}
+          options={{ headerShown: false, title: "" }}
+        />
+        <Stack.Screen
+          name="checkout"
+          component={Checkout}
+          options={{
+            headerShadowVisible: false,
+            title: "Meu Pedido",
+            headerTitleAlign: "center",
+            animation: "slide_from_bottom",
+            headerRight: () => {
+              return (
+                <TouchableOpacity onPress={() => alert("Ok")}>
+                  <Text style={{ color: COLORS.red }}>Limpar</Text>
+                </TouchableOpacity>
+              );
+            },
+          }}
+        />
+        <Stack.Screen
           name="order-detail"
           component={OrderDetail}
-          options={{ headerShown: false }}
+          options={{
+            headerShadowVisible: false,
+            title: "Detalhe do Pedido",
+            headerTitleAlign: "center",
+            animation: "slide_from_bottom",
+          }}
         />
         <Stack.Screen
           name="product-detail"
           component={ProductDetail}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="primary"
-          component={Primary}
           options={{ headerShown: false }}
         />
         <Stack.Screen
