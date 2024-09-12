c6be263fe1609d8d53edac85ab63ea735cb8a9e2
fernandoberbel <fberbel1000@gmail.com>
2024-09-12 01:11:44 -0300

    feat: created checkout page

---
+++ src/screens/checkout/checkout.style.js
@@ -0,0 +1,30 @@
+import { COLORS, FONT_SIZE } from "../../constants/theme.js";
+
+export const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 15,
+  },
+  valores: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 4,
+    marginBottom: 4,
+  },
+  valor: {
+    fontSize: FONT_SIZE.sm,
+    color: COLORS.dark_gray,
+  },
+  total: {
+    fontSize: FONT_SIZE.sm,
+    color: COLORS.dark_gray,
+    fontWeight: "bold",
+  },
+  containerBtn: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+};
